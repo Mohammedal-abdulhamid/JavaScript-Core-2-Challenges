e0ae9add277c7c9ba81b0bfcad20554656611a86
mohammed <mohammedalabdulhamid@gmail.com>
2021-08-05 12:58:19 +0100

    count words finished

--- challenge-count-words/script.js
+++ challenge-count-words/script.js
@@ -1,9 +1,23 @@
 function calculateWords(chapterOfABook) {
   const wordCount = {};
 
-  // Write your code in here
+  let split = chapterOfABook.split(" ");
+
+  split.forEach((word) => {
+    if (word !== "") {
+      if (wordCount[word] === undefined) {
+        wordCount[word] = 1;
+      } else {
+        wordCount[word] += 1;
+      }
+    }
+  });
 
   return wordCount;
+
+  // Write your code in here
+
+  return wordCount.Dracula;
 }
 
 calculateWords(getDraculaChapterOne());
